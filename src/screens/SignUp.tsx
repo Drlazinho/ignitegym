7107bbb7cb9a36d61eabea972a06c0aa2e8e8fc8
Lázaro Pimentel <tidesenvolvimento5@amvox.com.br>
2025-02-04 20:06:15 -0300

    Validação com Yup

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -13,6 +13,8 @@ import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 import { Controller, useForm } from "react-hook-form";
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 type FormDataProps = {
   name: string;
@@ -20,6 +22,16 @@ type FormDataProps = {
   password: string;
   password_confirm: string;
 };
+
+const signUpSchema = yup.object({
+  name: yup.string().required('Informe o nome'),
+  email: yup.string().required('Informe o e-mail').email('E-mail inválido'),
+  password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo menos 6 dígitos.'),
+  password_confirm: yup
+    .string()
+    .required('Confirme a senha')
+    .oneOf([yup.ref('password'), ''], 'A confirmação da senha não confere.'),
+});
 
 export function SignUp() {
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
@@ -32,7 +44,9 @@ export function SignUp() {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormDataProps>();
+  } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema)
+  });
 
   function handleSignUp({
     name,
@@ -69,14 +83,15 @@ export function SignUp() {
             <Controller
               control={control}
               name="name"
-              rules={{
-                required: "Informe o nome",
-              }}
+              // rules={{
+              //   required: "Informe o nome",
+              // }}
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="Nome"
                   onChangeText={onChange}
                   value={value}
+                  errorMessage={errors.name?.message}
                 />
               )}
             />
@@ -84,13 +99,13 @@ export function SignUp() {
             <Controller
               control={control}
               name="email"
-              rules={{
-                required: "Informe o email",
-                pattern: {
-                  value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: 'E-mail inválido'
-                }
-              }}
+              // rules={{
+              //   required: "Informe o email",
+              //   pattern: {
+              //     value:/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              //     message: 'E-mail inválido'
+              //   }
+              // }}
               render={({ field: { onChange, value } }) => (
                 <Input
                   placeholder="E-mail"
@@ -98,6 +113,7 @@ export function SignUp() {
                   autoCapitalize="none"
                   onChangeText={onChange}
                   value={value}
+                  errorMessage={errors.email?.message}
                 />
               )}
             />
@@ -113,6 +129,21 @@ export function SignUp() {
                   secureTextEntry
                   onChangeText={onChange}
                   value={value}
+                  errorMessage={errors.password?.message}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="password_confirm"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Confirme a senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  value={value}
+                  errorMessage={errors.password_confirm?.message}
                 />
               )}
             />
