3c39691629db3ea16d56c4d2467c02c029ee70da
Lázaro Pimentel <142930247+LazaroBonfim@users.noreply.github.com>
2025-02-06 19:39:48 -0300

    Contexto Auth

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import {  GluestackUIProvider } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config";
 import { Loading } from "@components/Loading";
 import { Routes } from "@routes/index";
+import { AuthContextProvider } from '@contexts/AuthContext'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
@@ -19,7 +20,10 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+      <AuthContextProvider>
+
       {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
     </GluestackUIProvider>
   );
 }
