96ff49ddb095eddd727934f4b109f731120cf7ba
Lázaro Pimentel <79115354+Drlazinho@users.noreply.github.com>
2025-03-01 20:14:25 -0300

    Validação Form Profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -13,16 +13,65 @@ import {
 import { Alert, TouchableOpacity } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system";
+import { Controller, useForm } from "react-hook-form";
+import { useAuth } from "@hooks/useAuth";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 import { useState } from "react";
 import { ToastMessage } from "@components/ToastMessage";
+
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  old_password: string;
+  confirm_password: string;
+};
+
+const profileSchema = yup.object({
+  name: yup
+    .string()
+    .required('Informe o nome'),
+  password: yup
+    .string()
+    .min(6, 'A senha deve ter pelo menos 6 dígitos.')
+    .nullable()
+    .transform((value) => !!value ? value : null),
+  confirm_password: yup
+    .string()
+    .nullable()
+    .transform((value) => !!value ? value : null)
+    .oneOf([yup.ref('password'), null], 'A confirmação de senha não confere.')
+    .when('password', {
+      is: (Field: any) => Field, 
+      then: yup
+        .string()
+        .nullable()
+        .required('Informe a confirmação da senha.')
+        .transform((value) => !!value ? value : null)
+    }),
+})
 
 export function Profile() {
   const [userPhoto, setUserPhoto] = useState(
     "https://github.com/drlazinho.png"
   );
+  const { user } = useAuth();
 
-  const toast = useToast()
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({ 
+    defaultValues: { 
+      name: user.name,
+      email: user.email
+    },
+    resolver: yupResolver(profileSchema) 
+  });
+
+  const toast = useToast();
+
+  async function handleProfileUpdate(data: FormDataProps) {
+    console.log(data);
+  }
 
   async function handleUserPhotoSelect() {
     try {
@@ -49,12 +98,12 @@ export function Profile() {
             render: ({ id }) => (
               <ToastMessage
                 id={id}
-                action='error'
-                title='Essa imagem é muito grande. Escolha uma de até 5MB.'
+                action="error"
+                title="Essa imagem é muito grande. Escolha uma de até 5MB."
                 onClose={() => toast.close(id)}
               />
-            )
-          })
+            ),
+          });
         }
 
         setUserPhoto(photoSelected.assets[0].uri);
@@ -66,13 +115,13 @@ export function Profile() {
 
   return (
     <VStack flex={1}>
-      <ToastMessage
+      {/* <ToastMessage
         id="1"
         title="Mensagem de exemplo"
         description="asdasdakjsd asdajksdbasjdhasd"
         action="success"
         onClose={() => {}}
-      />
+      /> */}
 
       <ScreenHeader title="Perfil" />
 
@@ -100,12 +149,30 @@ export function Profile() {
         </Center>
 
         <Center w="$full" gap="$4">
-          <Input placeholder="Nome" bg="$trueGray600" />
-          <Input
-            value="lazbonfim@hotmail.com"
-            placeholder="Nome"
-            bg="$trueGray600"
-            isReadOnly
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { value, onChange } }) => (
+              <Input
+                bg="$trueGray600"
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { value, onChange } }) => (
+              <Input
+                bg="$trueGray600"
+                placeholder="E-mail"
+                isReadOnly
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
           />
         </Center>
         <Heading
@@ -119,14 +186,46 @@ export function Profile() {
           Alterar senha
         </Heading>
         <Center w="$full" gap="$4">
-          <Input placeholder="Senha antiga" bg="$trueGray600" secureTextEntry />
-          <Input placeholder="Nova senha" bg="$trueGray600" secureTextEntry />
-          <Input
-            placeholder="Confirme a nova senha"
-            bg="$trueGray600"
-            secureTextEntry
+          <Controller
+            control={control}
+            name="old_password"
+            render={({ field: { onChange } }) => (
+              <Input
+                bg="$trueGray600"
+                placeholder="Senha antiga"
+                secureTextEntry
+                onChangeText={onChange}
+              />
+            )}
           />
-          <Button title="Atualizar" />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange } }) => (
+              <Input
+                bg="$trueGray600"
+                placeholder="Nova senha"
+                secureTextEntry
+                onChangeText={onChange}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="confirm_password"
+            render={({ field: { onChange } }) => (
+              <Input
+                bg="$trueGray600"
+                placeholder="Confirme a nova senha"
+                secureTextEntry
+                onChangeText={onChange}
+              />
+            )}
+          />{" "}
+          <Button
+            title="Atualizar"
+            onPress={handleSubmit(handleProfileUpdate)}
+          />
         </Center>
       </ScrollView>
     </VStack>
