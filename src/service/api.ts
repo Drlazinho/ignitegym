ee9982dfdf21e78f6ae7b53d1f686599df72bd2c
Lázaro Pimentel <79115354+Drlazinho@users.noreply.github.com>
2025-03-02 11:06:50 -0300

    Req em fila e buscando novo token

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,44 +1,88 @@
 
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios"
 
-import { AppError } from "@utils/AppError";
-import { storageAuthTokenGet } from '@storage/storageAuthToken'
+import { AppError } from "@utils/AppError"
+import { storageAuthTokenGet, storageAuthTokenSave } from '@storage/storageAuthToken'
 
-type SignOut = () => void;
+type SignOut = () => void
+
+type PromiseType = {
+  onSuccess: (token: string) => void
+  onFailure: (token: string) => void
+}
 
 type APIInstanceProps = AxiosInstance & {
-  registerInterceptTokenManager: (signOut: SignOut) => () => void;
+  registerInterceptTokenManager: (signOut: SignOut) => () => void
 }
 
 const api = axios.create({
   baseURL: 'http://192.168.1.9:3333'
-}) as APIInstanceProps;
+}) as APIInstanceProps
+
+let failedQueue: PromiseType[] = []
+let isRefreshing: boolean = false
 
 api.registerInterceptTokenManager = singOut => {
   const interceptTokenManager = api.interceptors.response.use((response) => response, async (requestError) => {
-    if(requestError?.response?.status == 401){
-      if(requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
-        const { refresh_token } = await storageAuthTokenGet();
+    if (requestError?.response?.status == 401) {
+      if (requestError.response.data?.message === 'token.expired' || requestError.response.data?.message === 'token.invalid') {
+        const { refresh_token } = await storageAuthTokenGet()
 
-        if(!refresh_token) {
-          singOut();
+        if (!refresh_token) {
+          singOut()
           return Promise.reject(requestError)
         }
+
+        const originalRequestConfig = requestError.config
+
+        if (isRefreshing) {
+          return new Promise((resolve, reject) => {
+            failedQueue.push({
+              onSuccess: (token: string) => {
+                originalRequestConfig.headers = { 'Authorization': `Bearer ${token}` }
+                resolve(api(originalRequestConfig))
+              },
+              onFailure: (error: AxiosError) => {
+                reject(error)
+              },
+            })
+          })
+        }
+
+        isRefreshing = true
+
+        return new Promise(async (resolve, reject) => {
+          try {
+            const { data } = await api.post('/sessions/refresh-token', { refresh_token });
+            await storageAuthTokenSave({ token: data.token, refresh_token: data.refresh_token})
+          } catch (error: any) {
+            failedQueue.forEach((request) => {
+              request.onFailure(error)
+            })
+
+            singOut()
+            reject(error)
+          } finally {
+            isRefreshing = false
+            failedQueue = []
+          }
+        }
+        )
       }
 
-      singOut();
+      singOut()
     }
 
-    if(requestError.response && requestError.response.data) {
+    if (requestError.response && requestError.response.data) {
       return Promise.reject(new AppError(requestError.response.data.message))
     } else {
       return Promise.reject(requestError)
     }
-  });
+  })
 
   return () => {
-    api.interceptors.response.eject(interceptTokenManager);
+    api.interceptors.response.eject(interceptTokenManager)
   }
 }
 
-export { api };
+export { api }
